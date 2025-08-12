cb2ff823409c12faf71c9254b9a2d381a5cd3dcd
Ignacio <ignamosconi@gmail.com>
2025-08-12 17:41:34 -0300

    Actualización - Logger en consola y registro en archivo txt

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,54 @@
 import { NestFactory } from '@nestjs/core';
 import { MarcaModule } from './marca/marca.module';
 import { ValidationPipe } from '@nestjs/common';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MarcaModule);
+  const app = await NestFactory.create(MarcaModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        // Formato legible en consola (estilo NestJS clásico)
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp(),
+            winston.format.printf(({ context, level, timestamp, message }) => {
+              const localTime = new Date(timestamp as string).toLocaleString('es-AR', {hour12: false,});
+              return `${localTime} [${context}] ${level}: ${message}`;
+            }),
+          ),
+        }),
 
-  //Validaciones de DTOs (ver comentario en el Post para crear marcas)
+        // Logs en archivo de texto (informativos)
+        new winston.transports.File({
+          filename: 'logs/app.log',
+          level: 'info',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json()
+          ),
+        }),
+
+        // Logs en archivo de errores
+        new winston.transports.File({
+          filename: 'logs/error.log',
+          level: 'error',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.json()
+          ),
+        }),
+      ],
+    }),
+  });
+
+  // Validaciones de DTOs. Ver comentario en el @Post (creación de marcas)
   app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }),
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   await app.listen(process.env.PORT ?? 3000);
--- src/marca/marca.service.ts
+++ src/marca/marca.service.ts
@@ -1,12 +1,19 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';   //Aprovechamos para importar el logger
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Marca } from './marca.entity';
 import { CreateMarcaDto } from './dto/create-marca.dto';
 import { UpdateMarcaDto } from './dto/update-marca.dto';
 
+
 @Injectable()
 export class MarcaService {
+
+  /*
+  LOGGER
+  Nos permitirá registrar todas las transacciones que se realicen en la aplicación
+  */
+  private readonly logger = new Logger(MarcaService.name);
 
   /*
   CONSTRUCTOR
@@ -26,19 +33,24 @@ export class MarcaService {
   CONSULTAS
   */
   hola(): string {
-    return 'hi mum!';
+    return "Hi mum!"
   }
 
   //Es async porque tenemos que esperar la consulta a la BD, y esto se hará en paralelo.
   async findAll(): Promise<Marca[]> {
+    this.logger.log("Buscando todas las marcas..."); //Lo anunciamos por consola.
     return await this.marcaRepository.find()
   }
 
   async findOne(id: number): Promise<Marca> {
+    this.logger.log(`Buscando la marca ID:${id}`)
     const marca = await this.marcaRepository.findOne({ where: { id } });
     if (!marca) {
-     throw new NotFoundException(`No se encontró la marca con ID ${id}`);
+      this.logger.error(`No se encontró la marca con ID ${id}`)   //Usamos .error en lugar de .log
+      throw new NotFoundException(`No se encontró la marca con ID ${id}`);
     }
+
+    this.logger.debug(`Marca ${marca?.nombre} encontrada`)
     return marca;
   }
 
@@ -51,6 +63,7 @@ export class MarcaService {
   nos permite chequear.
   */
   async crearMarca(createMarcaDto: CreateMarcaDto): Promise<Marca> {
+    this.logger.log(`Creando marca: ${createMarcaDto.nombre}`); //Lo anunciamos por consola.
     return this.marcaRepository.save(createMarcaDto)
   }
 
@@ -63,6 +76,7 @@ export class MarcaService {
     
     //Reemplazamos la marca anterior con la actualizada
     const marcaActualizada = Object.assign(marcaOriginal, updateMarcaDto);
+    this.logger.log(`Marca actualizada: ID ${id}, nuevo nombre: ${marcaActualizada.nombre}, nueva descripción: ${marcaActualizada.descripcion}`);
     return this.marcaRepository.save(marcaActualizada)  //No hace falta el await, porque es lo último que hacemos.
   }
 
@@ -75,6 +89,7 @@ export class MarcaService {
     const nombre = marcaOriginal.nombre
     //Si existe, la eliminamos. 
     await this.marcaRepository.remove(marcaOriginal)
+    this.logger.log(`Marca ${nombre} eliminada correctamente.`)
     return { message: `Marca ${nombre} eliminada correctamente` };
   }
 }
